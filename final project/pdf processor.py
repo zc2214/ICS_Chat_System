44b694fe76ae318b250f5cc8fedb37cda50c8ec2
James Chen <71540992+zc2214@users.noreply.github.com>
2021-11-27 23:36:21 +0800

    Add files via upload

---
+++ final project/pdf processor.py
@@ -0,0 +1,30 @@
+import os
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from shutil import copy2
+from pathlib import Path
+
+src_dir = Path(__file__).parent/"papers_oldDir"               #Local folder
+des_dir = Path(__file__).parent/'papers_newDir'               #new folder
+num = 0
+
+if not os.path.exists(des_dir):		#if no, creat one
+    os.makedirs(des_dir)
+
+if os.path.exists(src_dir):
+    dirs = os.listdir(src_dir)          
+    for dirc in dirs:                   #For every pdf in folder
+        pdf_reader = PdfFileReader(open(os.path.join(src_dir, dirc), 'rb'))      #Open and create a pdf file
+        paper_title = pdf_reader.getDocumentInfo().title                         #Get the title
+        print("num : %s" % num , paper_title)                                    # Show progress
+        num += 1
+        paper_title = str(paper_title)                                           #Title as str
+        
+        if paper_title.find('/') != -1:       #Error handling
+            new_paper_title = paper_title.replace('/','_')
+            paper_title = new_paper_title
+            copy2(os.path.join(src_dir, dirc), os.path.join(des_dir, paper_title) + '.pdf')
+        else:
+            copy2(os.path.join(src_dir, dirc), os.path.join(des_dir, paper_title) + '.pdf')
+        
+else:
+    print("Error")
